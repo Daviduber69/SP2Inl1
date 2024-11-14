4e2d616e40b48f32f5fc7972bb59604fc0156ab1
Daviduber69 <davhen0108@skola.goteborg.se>
2024-11-14 17:53:51 +0100

    assignment 4

---
+++ src/com/yrgo/advice/PerformanceTimingAdvice.java
@@ -0,0 +1,23 @@
+package com.yrgo.advice;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+
+
+public class PerformanceTimingAdvice {
+    public Object performTimingMeasurement (ProceedingJoinPoint method) throws Throwable {
+
+            long startTime =System.nanoTime();
+
+            try {
+                Object value= method.proceed();
+                return value;
+            }finally {
+                long endTime= System.nanoTime();
+                long timeTaken = endTime - startTime;
+                System.out.println("The method " +
+                        method.getSignature().getName() +" in the " +method.getTarget().getClass() + " took " + timeTaken
+                        /1000000 + " ms");
+            }
+        }
+    }
+
